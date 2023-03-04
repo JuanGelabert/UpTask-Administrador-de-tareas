d5c933506647d4c4c2bdf0661ae1c165ff35d7b8
Juan Gelabert <juanc.gelabert@gmail.com>
2023-03-04 13:06:07 -0300

    Commit inicial + Routing y vista login

---
+++ views/auth/login.php
@@ -0,0 +1,34 @@
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar sesión</p>
+
+        <form action="/" method="POST" class="formulario">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="ejemplo@mailbox.com"
+                />
+
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Ingrese su contraseña"
+                />
+            </div>
+            <input type="submit" value="Iniciar sesión" class="boton">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Aún no posees una cuenta? <span>Regístrate gratis</span></a>
+            <a href="/olvide">¿Olvidaste tu contraseña?</a>
+        </div>
+    </div> <!-- .contenedor-sm
+</div>
